acbddcad839d72908245d65783deb194d442a48e
zloslavakazov <113176791+zloslavakazov@users.noreply.github.com>
2022-09-10 11:37:57 +0500

    zloslavakazov/index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+function SendMessage() { 
+ const inputName = document.getElementById("name"); 
+ const inputEmail = document.getElementById("email"); 
+ const inputTel = document.getElementById("email"); 
+ if (inputName.value == "" && inputEmail.value == "" && inputTel.value == "" &&) { 
+ inputName.style.borderColor = "violet"; 
+ inputEmail.style.borderColor = "violet"; 
+ input.style.borderColor = "violet"; 
+ } 
+ }
